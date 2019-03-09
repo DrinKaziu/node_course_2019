80587d77ae1dc65ea9be5531920da3f1cbf863db
SnowSurfer <drinkaziu@gmail.com>
2019-03-09 16:34:16 -0500

    finish Global Modules lesson

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,6 +4,6 @@ const getNotes = require('./notes.js');
 const message = getNotes();
 
 console.log(message);
-console.log(chalk.green.inverse('Success!!!'));
+console.log(chalk.red.inverse.bold('Error!!!'));
 
 

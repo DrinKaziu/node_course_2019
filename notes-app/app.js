21ac48acbdb0b80d87af420c2e562bee2c92cd8c
SnowSurfer <drinkaziu@gmail.com>
2019-03-09 15:27:43 -0500

    fs.writeFileSync/fs.appendFileSync

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'My name is Nacho!!');
+// fs.writeFileSync('notes.txt', 'My name is Nacho!!');
+
+fs.appendFileSync('notes.txt', 'I am a cute dog');

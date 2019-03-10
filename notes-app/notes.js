69e9c29cc9f518dfdf6d242a419db954d434b0cc
SnowSurfer <drinkaziu@gmail.com>
2019-03-10 13:02:44 -0400

    finish Removing a Note lesson

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
 const getNotes = function() {
   return 'Your notes..'
@@ -24,6 +25,21 @@ const addNote = function(title, body) {
   }
 }
 
+const removeNote = function(title) {
+  const notes = loadNotes();
+
+  const notesToKeep = notes.filter(function(note) {
+    return note.title !== title
+  });
+
+  if (notes.length > notesToKeep.length) {
+    saveNotes(notesToKeep);
+    console.log(chalk.green('Note Removed'));
+  } else {
+    console.log(chalk.red('Note Not Found'));
+  }
+}
+
 const saveNotes = function(notes) {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync('notes.json', dataJSON);
@@ -41,5 +57,6 @@ const loadNotes = function() {
 
 module.exports = {
   getNotes: getNotes,
-  addNote: addNote
+  addNote: addNote,
+  removeNote: removeNote
 } 

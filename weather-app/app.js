628ea898f7603aaac98e1063de4cffcb2107a4de
SnowSurfer <drinkaziu@gmail.com>
2019-03-11 15:29:11 -0400

    finish Customizing HTTP Requests lesson

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,8 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/e79b022885f7770c971c38a56eba6e08/37.8267,-122.4233';
+const url = 'https://api.darksky.net/forecast/e79b022885f7770c971c38a56eba6e08/37.8267,-122.4233?units=si&lang=es';
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.currently);
-  
+request({ url: url, json: true }, (error, response) => {
+  console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees outside.`);
+  console.log(`There is a ${response.body.currently.precipProbability}% chance of precipitation`);
 });

1a739a23d35cffbb2ba453283398252c6cc5a37a
SnowSurfer <drinkaziu@gmail.com>
2019-03-12 13:22:39 -0400

    finish Callback Abstraction lesson

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,7 @@
 const request = require('request');
 
+const geocode = require('./utils/geocode')
+
 // const url = 'https://api.darksky.net/forecast/e79b022885f7770c971c38a56eba6e08/37.8267,-122.4233?units=us';
 
 // request({ url: url, json: true }, (error, response) => {
@@ -14,16 +16,23 @@ const request = require('request');
 // });
 
 // Geocoding - convert address to lat/long format
-const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic25vd3N1cmZlciIsImEiOiJjanQ0cXZoOGMxNjl1NDRsdnU4MmRzc2J0In0.UakM-dYXURfkPETckOVKVw&limit=1';
+// const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic25vd3N1cmZlciIsImEiOiJjanQ0cXZoOGMxNjl1NDRsdnU4MmRzc2J0In0.UakM-dYXURfkPETckOVKVw&limit=1';
 
-request({ url: geocodeUrl, json: true }, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to location services');
-  } else if (response.body.features.length === 0) {
-    console.log('Unable to find location. Please try again.');
-  } else {
-    const longitude = response.body.features[0].center[0];
-    const latitude = response.body.features[0].center[1]
-    console.log(`The coordinates for ${response.body.features[0].text} are Long: ${longitude}, Lat: ${latitude}`);
-  }
-});
+// request({ url: geocodeUrl, json: true }, (error, response) => {
+//   if (error) {
+//     console.log('Unable to connect to location services');
+//   } else if (response.body.features.length === 0) {
+//     console.log('Unable to find location. Please try again.');
+//   } else {
+//     const longitude = response.body.features[0].center[0];
+//     const latitude = response.body.features[0].center[1]
+//     console.log(`The coordinates for ${response.body.features[0].text} are Long: ${longitude}, Lat: ${latitude}`);
+//   }
+// });
+
+
+
+geocode('boston', (error, data) => {
+  console.log('Error: ', error);
+  console.log('Data: ', data);
+}) 

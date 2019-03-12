1bb7e0b4b132034b3b8dde5d9b2928f687cf3869
SnowSurfer <drinkaziu@gmail.com>
2019-03-12 15:35:44 -0400

    finish Object Distructuring lesson

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,33 @@
+// Object property shorthand
+const name = 'Nacho';
+const userAge = '6';
+
+const user = {
+  name,
+  userAge,
+  location: 'New York'
+}
+
+console.log(user.name);
+
+// Object destructuring 
+const product = {
+  label: 'Red Notebook',
+  price: 3,
+  stock: 211,
+  salePrice: undefined
+}
+
+// const label = product.label;
+// const price = product.price
+
+// const { label: productLabel, price, stock, salePrice, rating = 5 } = product; //we can rename the variable(label: productLabel)
+//                                                                               //or we can add a variable (rating = 5)
+
+// console.log(productLabel, price, stock, rating);
+
+const transaction = (type, {label, stock}) => {
+  console.log(type, label, stock);
+}
+
+transaction('order', product);

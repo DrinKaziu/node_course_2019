ca6a4635dae51738e4eb1ed3e73c510ee5d57b8c
SnowSurfer <drinkaziu@gmail.com>
2019-03-12 14:06:48 -0400

    finish Callback Abstraction Challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,7 @@
 const request = require('request');
 
-const geocode = require('./utils/geocode')
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 // const url = 'https://api.darksky.net/forecast/e79b022885f7770c971c38a56eba6e08/37.8267,-122.4233?units=us';
 
@@ -32,7 +33,12 @@ const geocode = require('./utils/geocode')
 
 
 
-geocode('boston', (error, data) => {
-  console.log('Error: ', error);
-  console.log('Data: ', data);
-}) 
+// geocode('boston', (error, data) => {
+//   console.log('Error: ', error);
+//   console.log('Data: ', data);
+// }); 
+
+forecast(-75.7088, 44.1545, (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
+});

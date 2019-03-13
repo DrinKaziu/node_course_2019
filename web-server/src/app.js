6c222fcf41cc9340dca9d35614ede2bc441509cb
SnowSurfer <drinkaziu@gmail.com>
2019-03-13 14:17:23 -0400

    finish Query String lesson

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -40,9 +40,15 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide a location'
+    });
+  }
   res.send({
     location: 'New York',
-    forecast: 'Cold'
+    forecast: 'Cold',
+    address: req.query.address
   });
 });
 

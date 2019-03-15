d43aff45c6ed1a7bc6c18bcc425a226fca7a4719
SnowSurfer <drinkaziu@gmail.com>
2019-03-15 16:26:33 -0400

    finish Insert Documents lesson

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -12,8 +12,53 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
   }
 
   const db = client.db(databaseName);
-  db.collection('users').insertOne({
-    name: 'Drin', 
-    age: 36
-  });
+
+  // db.collection('users').insertOne({
+  //   name: 'Drin', 
+  //   age: 36
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert user.');
+  //   }
+
+  //   console.log(result.ops);
+  // });
+
+  // db.collection('users').insertMany([
+  //   {
+  //     name: 'Nacho',
+  //     age: 6
+  //   },
+  //   {
+  //     name: 'Cooper',
+  //     age: 4
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert users.');
+  //   }
+
+  //   console.log(result.ops);
+  // })
+
+  db.collection('tasks').insertMany([
+    {
+      description: 'Take Drin for a walk',
+      completed: false
+    }, 
+    {
+      description: 'Ask Drin for treats',
+      completed: false
+    }, 
+    {
+      description: 'Take a nap',
+      completed: false
+    }
+  ], (error, result) => {
+    if (error) {
+      return console.log('Unable to insert task(s)');
+    }
+
+    console.log(result.ops);
+  })
 });

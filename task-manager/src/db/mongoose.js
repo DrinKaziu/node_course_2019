06f4fbb0ab2e9110cc2ade650ddd4c573db899b8
SnowSurfer <drinkaziu@gmail.com>
2019-03-17 19:42:36 -0400

    finish Creating a Mongoose Model lesson

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -14,13 +14,33 @@ const User = mongoose.model('User', {
   }
 });
 
-const me = new User({
-  name: 'Drin',
-  age: 'thirtysix'
+// const me = new User({
+//   name: 'Drin',
+//   age: 36
+// });
+
+// me.save().then(() => {
+//   console.log(me);
+// }).catch((error) => {
+//   console.log('Error: ', error);
+// });
+
+const Task = mongoose.model('Task', {
+  description: {
+    type: String
+  }, 
+  completed: {
+    type: Boolean
+  }
 });
 
-me.save().then(() => {
-  console.log(me);
+const taskOne = new Task({
+  description: 'Take Drin for a walk',
+  completed: false
+});
+
+taskOne.save().then((result) => {
+  console.log(result);
 }).catch((error) => {
-  console.log('Error: ', error);
+  console.log(error);
 });

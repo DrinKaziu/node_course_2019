934f6a4d66affbcae7b1341ab3a2c301e776ab73
SnowSurfer <drinkaziu@gmail.com>
2019-03-18 17:54:59 -0400

    Resource Creation Endpoints II

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -4,25 +4,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true
 });
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    trim: true,
-    required: true
-  }, 
-  completed: {
-    type: Boolean,
-    default: false
-  }
-});
-
-const task = new Task({
-  description: 'Ask for treats!!'
-});
-
-task.save().then((result) => {
-  console.log(result);
-}).catch((error) => {
-  console.log(error);
-});

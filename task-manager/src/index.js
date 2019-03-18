afaa5a87a2b0b7f6800327d2a615ea211e4eba0e
SnowSurfer <drinkaziu@gmail.com>
2019-03-18 19:09:03 -0400

    Resource Reading Endpoints II

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -37,8 +37,8 @@ app.get('/users/:id', (req, res) => {
     res.send(user);
   }).catch((e) => {
     res.status(500).send();
-  })
-})
+  });
+});
 
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
@@ -47,8 +47,30 @@ app.post('/tasks', (req, res) => {
     res.status(201).send(task);
   }).catch((e) => {
     res.status(400).send(e);
-  })
-})
+  });
+});
+
+app.get('/tasks', (req, res) => {
+  Task.find({}).then((tasks) => {
+    res.send(tasks);
+  }).catch((e) => {
+    res.status(500).send();
+  });
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id).then((task) => {
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  }).catch((e) => {
+    res.status(500).send();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);

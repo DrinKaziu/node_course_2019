2e497fba9c1cb43f81d66492eb16e97b5e935611
SnowSurfer <drinkaziu@gmail.com>
2019-03-19 15:34:34 -0400

    Update Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -61,6 +61,22 @@ app.get('/users/:id', async (req, res) => {
   // });
 });
 
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'age', 'email', 'password'];
+  const isValidUpdate = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidUpdate) return res.status(400).send({ error: 'Invalid update(s).'});
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+    if (!user) return res.status(404).send();
+    res.send(user);
+  } catch(e) {
+    res.status(400).send(e);
+  }
+})
+
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
 
@@ -92,6 +108,22 @@ app.get('/tasks/:id', async (req, res) => {
     res.status(500).send();
   }
 });
+
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidUpdate = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidUpdate) return res.status(400).send({ error: 'Invalid Update(s)'});
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+    if (!task) return res.status(404).send();
+    res.send(task);
+  } catch(e) {
+    res.status(400).send(e);
+  } 
+})
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);

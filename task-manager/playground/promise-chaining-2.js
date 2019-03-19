c3179a26dd3c9b84f5e7f32b9c543b43a7a90e4b
SnowSurfer <drinkaziu@gmail.com>
2019-03-18 20:38:48 -0400

    async/await II

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,12 +1,24 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5c901e9e1d3939dd9b7b855d').then((task) => {
-  console.log(task);
+// Task.findByIdAndDelete('5c901e9e1d3939dd9b7b855d').then((task) => {
+//   console.log(task);
 
-  return Task.countDocuments({ completed: false }); 
-}).then((result) => {
-  console.log(result);
+//   return Task.countDocuments({ completed: false }); 
+// }).then((result) => {
+//   console.log(result);
+// }).catch((e) => {
+//   console.log(e);
+// });
+
+const deleteTaskAndCount = async (id) => {
+  const task = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count; 
+}
+
+deleteTaskAndCount('5c900ef64bc2a3d9f19c2bfd').then((count) => {
+  console.log(count);
 }).catch((e) => {
   console.log(e);
 });
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,15 +1,24 @@
 require('../src/db/mongoose');
-
 const User = require('../src/models/user');
 
+// User.findByIdAndUpdate('5c8ef9c0a76f35d4f683c8c6', { age: 6 }).then((user) => {
+//   console.log(user);
 
+//   return User.countDocuments({ age: 6 });
+// }).then((result) => {
+//   console.log(result)
+// }).catch((e) => {
+//   console.log(e);
+// });
 
-User.findByIdAndUpdate('5c8ef9c0a76f35d4f683c8c6', { age: 6 }).then((user) => {
-  console.log(user);
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count; 
+}
 
-  return User.countDocuments({ age: 6 });
-}).then((result) => {
-  console.log(result)
+updateAgeAndCount('5c8ef9c0a76f35d4f683c8c6', 7).then((count) => {
+  console.log(count);
 }).catch((e) => {
   console.log(e);
 });

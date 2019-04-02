360c7b0b640ef4667fb837e5c9541b51d9cfe658
SnowSurfer <drinkaziu@gmail.com>
2019-04-02 18:42:46 -0400

    Accepting Authentication Tokens

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -4,6 +4,7 @@ const Task = require('./models/task');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -12,13 +13,13 @@ const port = process.env.PORT || 3000;
 //   next();
 // });
 
-app.use((req, res, next) => {
-  if(req.method === 'GET') {
-    res.status(503).send('Under construction...');
-  } else {
-    next();
-  }
-})
+// app.use((req, res, next) => {
+//   if(req.method === 'GET') {
+//     res.status(503).send('Under construction...');
+//   } else {
+//     next();
+//   }
+// })
 
 app.use(express.json());
 app.use(userRouter);
